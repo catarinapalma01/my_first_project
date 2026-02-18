2a8aa63999abf54c12f1648342a08167af6dfe8f
catarinapalma01 <catarinapalma01@gmail.com>
2026-02-18 18:47:13 +0000

    I am plotting geodata now

--- geopandas_test.py
+++ geopandas_test.py
@@ -1,3 +1,12 @@
+# Import necessary libraries
 import geopandas as gpd
+import matplotlib.pyplot as plt
 
-print("hello")
+# Load a GeoJSON file containing geospatial data
+url = "https://raw.githubusercontent.com/nmota/caop_GeoJSON/master/ContinenteConcelhos.geojson"
+portugal = gpd.read_file(url)
+
+fig, ax = plt.subplots(figsize=(10, 10))
+
+# Plot the geospatial data
+portugal.plot(ax=ax, color='lightblue', edgecolor='black')
